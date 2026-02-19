90d948903f0a7e298cdcefb197e4d4a117c8db8b
Lis Torres <spelive16@gmail.com>
2026-02-19 18:32:44 +0100

    build: init spring boot project

---
+++ src/main/java/com/lis/pomodoro/PomodoroTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.lis.pomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lis/pomodoro/PomodoroTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lis.pomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
